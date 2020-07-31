4f60ff8b098d4f33899a08cb8c65fed9b914bae4
Bannock <Timothy.J.Bannock@disney.com>
2020-07-31 14:29:28 -0700

    Uploading Mod 3 Challenge.

--- Lessons/Python_practice.py
+++
@@ -1,9 +0,0 @@
-counties = ["Arapahoe", "Denver", "Jefferson"]
-if "El Paso" in counties:
-	print("El Paso is in the list of counties.")
-else:
-	print("El Paso is not in the list of counties.")
-
-for county in counties:
-	print(county)
-
